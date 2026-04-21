6e117583e0ca74574a7ae59fce084ae5985a4287
MDATIK-3 <atikh4335@gmail.com>
2026-04-22 01:58:45 +0600

    made greenish color

--- src/components/shared/AppFooter.vue
+++ src/components/shared/AppFooter.vue
@@ -1,6 +1,6 @@
-<template>
+ish<template>
   <q-footer class="bg-grey-9 text-white text-center q-py-md">
-    <div>&copy; 2024 Restaurant POS. All rights reserved.</div>
+    <div>&copy; 2026 Restaurant POS. All rights reserved.</div>
   </q-footer>
 </template>
 
--- src/components/shared/ThemeSwitcher.vue
+++ src/components/shared/ThemeSwitcher.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-toggle v-model="$q.dark" icon="dark_mode" checked-icon="light_mode" unchecked-icon="dark_mode" color="amber" />
+  <q-toggle v-model="$q.dark" icon="dark_mode" checked-icon="light_mode" unchecked-icon="dark_mode" color="green" />
 </template>
 
 <script setup>
